18947bbc2ef969ee386331b9338f8e6d50b20df2
Mariana Daruix <mariana.daruix@gmail.com>
2023-01-16 14:51:15 -0300

    adiciona arquivos a EX03 S02 M01

--- Ex03/app.js
+++ Ex03/app.js
@@ -1,5 +1,17 @@
 console.log('EX03-S03-M1');
+/*
+function concatena(arrayA, arrayB){
+    const novoArray = [...arrayA,...arrayB];
+    return novoArray;
+};
+*/
 
+const concatena = (arrayA, arrayB) => {   //Arrow function q concatena as duas array A e B
+    return [...arrayA,...arrayB];
+};
 
+const novoArray = concatena([1,2,3],[4,5,6]);
+console.log(novoArray);
+//novo array deve ser [1,2,3,4,5,6]
 
 
